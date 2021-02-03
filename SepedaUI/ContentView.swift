429ce16fc5ed33d79707b08add9b2ca0765a8d0c
m0zl4 <zayuka040104@gmail.com>
2021-02-03 10:14:58 +0700

    Initial Commit

---
+++ SepedaUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SepedaUI
+//
+//  Created by muhammad abdul latief on 03/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
